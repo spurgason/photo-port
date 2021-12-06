c64814466fb4a3b7bf641bbaf76ee57a07964f1c
spurgason <shanepurgason.98@gmail.com>
2021-12-06 16:15:44 -0500

    nav test pass

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -19,18 +19,18 @@ function Nav() {
     function categorySelected() {
         console.log("clicked");
     }
-    
+
     return (
         <header>
             <h2>
-                <a href="/">
+                <a data-testid="link" href="/">
                     <span role="img" aria-label="camera">📸</span> Oh Snap!
                 </a>
             </h2>
             <nav>
                 <ul className="flex-row">
                     <li className="mx-2">
-                        <a href="#about">About Me</a>
+                        <a data-testid="about" href="#about">About Me</a>
                     </li>
                     <li>
                         <span>Contact</span>
