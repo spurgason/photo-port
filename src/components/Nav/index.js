17d874b05035005890c426181243abe205baf6f9
spurgason <shanepurgason.98@gmail.com>
2021-12-07 13:30:14 -0500

    contact now renders by itself on nav click

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -6,6 +6,8 @@ function Nav(props) {
         categories = [],
         setCurrentCategory,
         currentCategory,
+        contactSelected,
+        setContactSelected
     } = props;
 
     useEffect(() => {
@@ -26,22 +28,18 @@ function Nav(props) {
             <nav>
                 <ul className="flex-row">
                     <li className="mx-2">
-                        <a
-                             data-testid="about"
-                            href="#about"
-                        >
-                            About Me
-                        </a>
+                        <a data-testid="about" href="#about" onClick={() => setContactSelected(false)}> About Me </a>
                     </li>
-                    <li>
-                        <span>Contact</span>
+                    <li className = {`mx-2 ${contactSelected && 'navActive'}`}>
+                        <span onClick={() => setContactSelected(true)}>Contact</span>
                     </li>
                     {categories.map((category) => (
                         <li className={`mx-1 ${currentCategory.name === category.name && 'navActive'
                             }`} key={category.name}>
                             <span
                                 onClick={() => {
-                                    setCurrentCategory(category)
+                                    setCurrentCategory(category);
+                                    setContactSelected(false);
                                 }}
                             >
                                 {capitalizeFirstLetter(category.name)}
